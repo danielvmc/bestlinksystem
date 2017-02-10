bbccb2079fe0b0932c2d85efc2967a908c4ef5bd
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-02-10 18:52:01 +0700

    fix

--- app/Http/Controllers/LinksController.php
+++ app/Http/Controllers/LinksController.php
@@ -57,7 +57,7 @@ class LinksController extends Controller
             'user_name' => auth()->user()->name,
         ]);
 
-        return redirect('links');
+        return back()->withInput(request()->all());
     }
 
     public function show($link)
