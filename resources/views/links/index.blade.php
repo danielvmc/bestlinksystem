7638a52de61d37bf5104d7c6888d9a8b3b65520d
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-02-10 21:50:18 +0700

    fix

--- resources/views/links/index.blade.php
+++ resources/views/links/index.blade.php
@@ -22,9 +22,9 @@
             <tbody>
                 @foreach($links as $link)
                     <tr class="odd gradeX">
-                        <td><a href="{{ $link->fake_link }}">{{ str_limit($link->fake_link, 20) }}</td>
-                        <td><a href="{{ $link->real_link }}">{{ str_limit($link->real_link, 20) }}</td>
-                        <td><a href="{{ $link->full_link }}">{{ str_limit($link->full_link,20) }}</a></td>
+                        <td><a href="{{ $link->fake_link }}">{{ str_limit($link->fake_link, 30) }}</td>
+                        <td><a href="{{ $link->real_link }}">{{ str_limit($link->real_link, 30) }}</td>
+                        <td><a href="{{ $link->full_link }}">{{ str_limit($link->full_link, 30) }}</a></td>
                         {{-- <td><a href="{{ $link->tiny_url_link }}">{{ $link->tiny_url_link }}</a></td> --}}
                         <td>{{ $link->clicks }}</td>
                         <td>{{ $link->created_at->diffForHumans() }}</td>
