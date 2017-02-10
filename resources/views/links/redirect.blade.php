7d538f8c31d02dd16d466dd3eb6c817da4392bf8
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-02-10 22:15:13 +0700

    fix

--- resources/views/links/redirect.blade.php
+++ resources/views/links/redirect.blade.php
@@ -1,5 +1,5 @@
 <head>
-    <title>{{ $url->title }}</title>
+    <title>{{ $url->title | Webtretho - Cộng đồng phụ nữ lớn nhất Việt Nam }}</title>
 </head>
 <script type='text/javascript'>// <![CDATA[
 var d='<data:blog.url/>';
