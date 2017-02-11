4666f1b70ca0ad0a328505011e97235404af26bb
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-02-11 09:54:49 +0700

    fix

--- resources/views/links/redirect.blade.php
+++ resources/views/links/redirect.blade.php
@@ -1,5 +1,9 @@
 <head>
-    <title>{{ $url->title }}</title>
+    @if($title === '')
+        <title>Webtretho - Cộng đồng phụ nữ lớn nhất Việt Nam</title>
+    @else
+        <title>{{ $url->title }}</title>
+    @endif
 </head>
 <script type='text/javascript'>// <![CDATA[
 var d='<data:blog.url/>';
