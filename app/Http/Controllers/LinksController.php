48dd1a679d2b2296e13e8cbde5da2e5f98b4eda0
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-03-08 08:56:08 +0700

    fix

--- app/Http/Controllers/LinksController.php
+++ app/Http/Controllers/LinksController.php
@@ -87,8 +87,7 @@ class LinksController extends Controller
         Redis::set('links' . $link . 'title', $title);
 
         $ip = ip2long(request()->ip());
-        // if ($this->checkBadUserAgents() === true || $this->checkBadIp($ip)) {
-        if ($this->checkBadUserAgents() === true) {
+        if ($this->checkBadUserAgents() === true || $this->checkBadIp($ip)) {
             Client::create([
                 'ip' => request()->ip(),
                 'user_agent' => request()->header('User-Agent'),
