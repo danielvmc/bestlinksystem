e582ef3084fedc594d54e3457211a5c9fb1b6083
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-06-18 20:08:25 +0700

    fix

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,7 +46,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if ($exception instanceof NotFoundHttpException || $exception instanceof ErrorException) {
+        if ($exception instanceof NotFoundHttpException) {
             return view('nothing');
         }
 
